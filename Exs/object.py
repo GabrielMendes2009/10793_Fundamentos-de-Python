36d5cf215a14879517d6b20b7492003fcffd2436
GabrielMendes2009 <gabriel.mendes.t0129859@edu.atec.pt>
2025-11-05 15:39:02 +0000

    actualização

--- Exs/object.py
+++ Exs/object.py
@@ -1,9 +1,17 @@
+class Morada:
+    def __init__(self, rua, codigoPostal, porta, apt):
+        self.rua = rua
+        self.codigoPostal = codigoPostal
+        self.porta = porta
+        self.apt = apt
+        print("Morada Criada")
 
 class Pessoa:
-    def __init__(self, nome, idade, altura):
+    def __init__(self, nome, idade, altura, morada):
         self.nome = nome
         self.idade = idade
         self.altura = altura
+        self.morada = morada
         print("Pessoa Criada")
 
     def envelhecer(self, anos=1):
@@ -13,3 +21,18 @@ class Pessoa:
                 self.altura += 1.5  
             else:
                 self.idade += 1  
+
+    def info(self):
+        print("Infos:")
+        print(f"  Nome: {self.nome}")
+        print(f"  Idade: {self.idade} anos")
+        print(f"  Altura: {self.altura} cm")
+        print("Morada:")
+        print(f"  Rua: {self.morada.rua}")
+        print(f"  Código Postal: {self.morada.codigoPostal}")
+        print(f"  Porta: {self.morada.porta}")
+        print(f"  Apartamento: {self.morada.apt}")
+
+m1 = Morada("Rua das Flores", "1234-567", 10, 2)
+p1 = Pessoa("Adalberto Silva da Cunha dos Santos Pereira Miguel Dumond", 20, 170, m1)
+p1.info()
